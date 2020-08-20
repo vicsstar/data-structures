bca9990ef871b230631de2448b93c193ca629142
Victor Igbokwe <vicsstar@yahoo.com>
2020-08-20 04:22:54 +0300

    Restructure files and folders

---
+++ go/tests/DynamicArrayTest.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+// TestSize tests size of dynamic array
+func TestSize(t *testing.T) {
+	var dynArray DynamicArray = New(10)
+}
